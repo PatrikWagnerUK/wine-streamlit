e22aa7e666a5d7b94275327dc91de874fcdc8bd7
Patrik <patrikwagner.uk@gmail.com>
2021-09-22 14:15:25 +0200

    Copy update 3

--- Wine_data_prep.py
+++ Wine_data_prep.py
@@ -29,6 +29,8 @@ sig_kern = data["model"]
 ## Creating function to display streamlit page
 def show_page():
     st.title('Wine Recommendation Generator 1.0')
+    st.write('Built by: Patrik Wagner')
+    st.write('')
     st.write('Finding you the highest ranking and most relevant Italian wine recommendations. Curated by a Natural Language Processing algorithm backed by 6,000+ sommelier reviews.')
     st.markdown("____")
 
